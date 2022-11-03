a838ad82b1dd59c9a52d133287ed03f63c3ab5b6
Serhii Petrash <serhiipetrash@gmail.com>
2022-11-03 12:55:01 +0200

    create Navbar, Footer with windicss

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="w-full flex justify-between items-center mx-auto p-2 mb-4 shadow-md  bg-green-600">
+    <div class="flex items-center gap-2" text="xl white">
+      <NuxtLink class="icon-btn text-3xl" i-carbon-campsite to="/" />
+      <NuxtLink to="/" class="font-bold text-white uppercase text-3xl">
+        home
+      </NuxtLink>
+      <DarkToggle />
+    </div>
+
+    <nav class="flex gap-4" text="white">
+      <NuxtLink to="/" class="font-bold uppercase">
+        home
+      </NuxtLink>
+      <NuxtLink to="/" class="font-bold uppercase">
+        item-1
+      </NuxtLink>
+      <NuxtLink to="/" class="font-bold uppercase">
+        item-2
+      </NuxtLink>
+      <NuxtLink to="/old" class="font-bold uppercase">
+        old
+      </NuxtLink>
+    </nav>
+  </header>
+</template>
---
+++ components/MyFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    text="xl white"
+    mt-4
+    flex
+    justify-center
+    gap-3
+    bg-green-600
+    p-2
+  >
+    <NuxtLink i-carbon-campsite to="/" />
+    <a i-carbon-logo-github href="https://github.com/antfu/vitesse-nuxt3" target="_blank" />
+    <DarkToggle />
+  </div>
+</template>
